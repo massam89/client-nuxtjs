2ffe09ea79e3935ceec8eaf6bb48d1db4e12e461
Masoud <mas.sam1368@gmail.com>
2021-10-06 17:14:57 +0330

    fix layout problem by change nuxt version and refresh server

---
+++ layouts/noNav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="container">
+            <NuxtLink to="/">Go Back</NuxtLink>
+        </div>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+  
+</style>
