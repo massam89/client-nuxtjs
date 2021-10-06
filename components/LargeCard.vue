0279219177f52c4f202968e3d9594499e320e542
Masoud <mas.sam1368@gmail.com>
2021-10-06 16:08:51 +0330

    add vuex and getter

--- components/LargeCard.vue
+++ components/LargeCard.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="card">
+    <NuxtLink :to="`/products/${card.id}`" class="card">
         <img class="image" :src="require(`@/assets/images/${card.image || 'fe1.jpg'}`)" alt="">
         <h3 class="header">{{ card.title }}</h3>
         <p class="snippet">
             {{card.snippet }}
         </p>
-    </div>
+    </NuxtLink>
 </template>
 
 <script>
@@ -30,6 +30,10 @@ export default {
     .header {
         font-size: 1.15rem;
         margin-top: 0.4rem;
+    }
+    .header:hover {
+        color:black;
+        text-decoration: none;
     }
     .snippet {
         color: grey
---
+++ pages/my-items.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>my Item page</div>
+</template>
+
+<script>
+export default {
+ layout: 'noNav'
+}
+</script>
+
+<style scoped>
+
+</style>
